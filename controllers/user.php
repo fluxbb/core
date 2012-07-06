a6578da0de3c7b406d51f6bfe7971c53526e1f07
Alexander Cogneau <alexander.cogneau@hotmail.be>
2012-07-06 10:29:47 +0200

    Added the user list
    Signed-off-by: Alexander Cogneau <alexander.cogneau@hotmail.be>

--- controllers/user.php
+++ controllers/user.php
@@ -1,8 +1,8 @@
 <?php
 use fluxbb\User;
-class FluxBB_User_Controller extends FluxBB_BaseController
+class FluxBB_User_Controller extends Base_Controller
 {	
-	public function get_profile($id = 0)
+	public function action_profile($id = 0)
 	{
 		$user = User::where('id', '=', $id)->first();
 		if(!empty($id) && !empty($user)) //If no user defined or if user doesn't exist
@@ -12,5 +12,11 @@ class FluxBB_User_Controller extends FluxBB_BaseController
 		}
 		else
 		{ return "User not found";}
+	}
+	public function action_list()
+	{
+		$users = DB::table('users')->paginate(20);
+		return View::make('fluxbb::user.list')
+				->with('users', $users);
 	}
 }
--- routes.php
+++ routes.php
@@ -4,4 +4,5 @@ Route::get('(:bundle)/forum/(:num)', 'fluxbb::home@forum');
 Route::get('(:bundle)/topic/(:num)', 'fluxbb::home@topic');
 Route::get('(:bundle)/post/(:num)', 'fluxbb::home@post');
 Route::get('(:bundle)', 'fluxbb::home@index');
-Route::get('(:bundle)/profile/(:num)', "fluxbb::user@profile");
+Route::get('(:bundle)/profile/(:num)', array('as' => 'profile', 'uses' => "fluxbb::user@profile"));
+Route::get('(:bundle)/user/list', "fluxbb::user@list");
