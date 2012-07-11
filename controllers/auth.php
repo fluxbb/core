6dff3fbdc08d6cd53c358a88cce1ca17aee06ab0
Franz Liedke <franz@develophp.org>
2012-07-11 13:05:05 +0200

    Fix incorrect check for user being logged in when trying to login.

--- controllers/auth.php
+++ controllers/auth.php
@@ -27,13 +27,15 @@ use fluxbb\User;
 
 class FluxBB_Auth_Controller extends FluxBB_BaseController
 {
+	
 	public function get_login()
 	{
-		if ($this->user->is_member())
+		if ($this->user()->is_member())
 		{
 			return Redirect::to_action('fluxbb::home@index');
 		}
-		
+
+		// TODO: Might want to make this dynamic
 		$redirect_url = URL::to_action('fluxbb::home@index');
 
 		return View::make('fluxbb::auth.login')
