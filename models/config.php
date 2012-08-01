290beb8d37f9af6be0ea18530bc144dbbb61ac50
Franz Liedke <franz@develophp.org>
2012-08-01 23:20:03 +0200

    #704: Implement setting and saving config values.

--- models/config.php
+++ models/config.php
@@ -34,36 +34,38 @@ class Config
 
 	protected static $data = array();
 
-	protected static function data()
+	protected static $original = array();
+
+	protected static function load_data()
 	{
-		if (!static::$loaded)
+		if (static::$loaded)
 		{
-			static::$data = Cache::remember('fluxbb.config', function()
-			{
-				$data = DB::table('config')->get();
-				$cache = array();
-
-				foreach ($data as $row)
-				{
-					$cache[$row->conf_name] = $row->conf_value;
-				}
-
-				return $cache;
-			}, 24 * 60);
-
-			static::$loaded = true;
+			return;
 		}
+		
+		static::$data = static::$original = Cache::remember('fluxbb.config', function()
+		{
+			$data = DB::table('config')->get();
+			$cache = array();
 
-		return static::$data;
+			foreach ($data as $row)
+			{
+				$cache[$row->conf_name] = $row->conf_value;
+			}
+
+			return $cache;
+		}, 24 * 60);
+
+		static::$loaded = true;
 	}
 
 	public static function get($key, $default = null)
 	{
-		$data = static::data();
+		static::load_data();
 
-		if (array_key_exists($key, $data))
+		if (array_key_exists($key, static::$data))
 		{
-			return $data[$key];
+			return static::$data[$key];
 		}
 
 		return $default;
@@ -78,5 +80,55 @@ class Config
 	{
 		return static::get($key, 0) == 0;
 	}
+
+	public static function set($key, $value)
+	{
+		static::load_data();
+
+		static::$data[$key] = $value;
+	}
+
+	public static function delete($key)
+	{
+		static::load_data();
+
+		unset(static::$data[$key]);
+	}
+
+	public static function save()
+	{
+		// New and changed keys
+		$changed = array_diff_assoc(static::$data, static::$original);
+
+		$insert_values = array();
+		foreach ($changed as $name => $value)
+		{
+			if (!array_key_exists($name, static::$original))
+			{
+				$insert_values[] = array(
+					'conf_name'		=> $name,
+					'conf_value'	=> $value,
+				);
+
+				unset($changed[$name]);
+			}
+		}
+
+		DB::table('config')->insert($insert_values);
+
+		foreach ($changed as $name => $value)
+		{
+			DB::table('config')->where_conf_name($name)->update(array('conf_value' => $value));
+		}
+
+		// Deleted keys
+		$deleted_keys = array_keys(array_diff_key(static::$original, static::$data));
+		DB::table('config')->where_in('conf_name', $deleted_keys)->delete();
+
+		// No need to cache old values anymore
+		static::$original = static::$data;
+	}
+
+	// TODO: Do we need another function to clear the cache here?
 
 }
