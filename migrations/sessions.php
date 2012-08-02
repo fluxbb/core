b36a10c475fdc6ab88f745d7ed8132f1bc63b8a7
Franz Liedke <franz@develophp.org>
2012-08-03 00:56:49 +0200

    #306: Create migration for sessions database table.

---
+++ migrations/sessions.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * FluxBB - fast, light, user-friendly PHP forum software
+ * Copyright (C) 2008-2012 FluxBB.org
+ * based on code by Rickard Andersson copyright (C) 2002-2008 PunBB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public license for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category	FluxBB
+ * @package		Core
+ * @copyright	Copyright (c) 2008-2012 FluxBB (http://fluxbb.org)
+ * @license		http://www.gnu.org/licenses/gpl.html	GNU General Public License
+ */
+
+class FluxBB_Migration_Sessions
+{
+
+	public function up()
+	{
+		Schema::table('sessions', function($table)
+		{
+			$table->create();
+
+			$table->string('id', 64);
+			$table->integer('user_id')->default(1);
+			$table->integer('created')->default(0);
+			$table->integer('last_visit')->default(0);
+			$table->string('last_ip', 200)->default('0.0.0.0');
+			$table->text('data');
+
+			$table->primary('id');
+			$table->index('user_id');
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('sessions');
+	}
+
+}
