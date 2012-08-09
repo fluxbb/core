485d525ee1d3c4d75efc601c0a1b596752a0f0db
daris <daris91@gmail.com>
2012-08-09 16:42:38 +0200

    Initial footer version

--- language/en/common.php
+++ language/en/common.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 return array(
 
@@ -16,7 +16,7 @@ return array(
 	'password'		=> 'Password',
 	'email'			=> 'Email',
 	'required'		=> 'Required',
-	
+
 	'topics'		=> 'Topics',
 	'link_to'		=> 'Link to:', // As in "Link to: http://fluxbb.org/"
 	'empty_board'	=> 'Board is empty.',
@@ -33,18 +33,30 @@ return array(
 	'by'			=> 'by :author',
 	'never'			=> 'Never',
 	'important'		=> 'Important information',
-	
+
 	'replies'		=> 'Replies',
 	'views'			=> 'Views',
 	'last_post'		=> 'Last Post',
-	
+
 	'topics'		=> 'Topics',
 	'posts'			=> 'Posts',
-	
+
 	'today'			=> 'Today',
 	'yesterday'		=> 'Yesterday',
 	'never'			=> 'Never',
 
 	'login_to_view'	=> 'You need to login to view this page.',
 
+	// Stuff for the page footer
+	'board_footer'						=>	'Board footer',
+	// 'Jump to'							=>	'Jump to',
+	// 'Go'								=>	' Go ', // Submit button in forum jump
+	// 'Moderate topic'					=>	'Moderate topic',
+	// 'Move topic'						=>	'Move topic',
+	// 'Open topic'						=>	'Open topic',
+	// 'Close topic'						=>	'Close topic',
+	// 'Unstick topic'						=>	'Unstick topic',
+	// 'Stick topic'						=>	'Stick topic',
+	// 'Moderate forum'					=>	'Moderate forum',
+	'powered_by'						=>	'Powered by :link',
 );
