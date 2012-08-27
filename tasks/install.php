42a86ea13c81bde21abb9b6910858f6b02d78859
Franz Liedke <franz@develophp.org>
2012-08-27 16:56:10 +0200

    Create a simple CLI task for installing FluxBB database tables.
    Related to #695 and #703.

---
+++ tasks/install.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * FluxBB - fast, light, user-friendly PHP forum software
+ * Copyright (C) 2008-2012 FluxBB.org
+ * based on code by Rickard Andersson copyright (C) 2002-2008 PunBB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public license for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category	FluxBB
+ * @package		Core
+ * @copyright	Copyright (c) 2008-2012 FluxBB (http://fluxbb.org)
+ * @license		http://www.gnu.org/licenses/gpl.html	GNU General Public License
+ */
+
+use fluxbb\CLI\Task;
+
+class FluxBB_Install_Task extends Task
+{
+	
+	public function run($arguments = array())
+	{
+		foreach (new FilesystemIterator($this->path()) as $file)
+		{
+			$migration = basename($file->getFileName(), '.php');
+
+			$this->log('Install '.$migration.'...');
+
+			$class = 'FluxBB_Install_'.Str::classify($migration);
+			include_once $file;
+
+			$instance = new $class;
+			$instance->up();
+		}
+	}
+
+	protected function path()
+	{
+		return Bundle::path('fluxbb').'migrations'.DS.'install'.DS;
+	}
+
+}
