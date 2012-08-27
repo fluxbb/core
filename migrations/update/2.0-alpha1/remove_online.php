9bf85670348d2aa1352068fa6e0b7728e2d78a55
Franz Liedke <franz@develophp.org>
2012-08-27 15:29:56 +0200

    #390: Add migration for removing online table in 2.0.

---
+++ migrations/update/2.0-alpha1/remove_online.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * FluxBB - fast, light, user-friendly PHP forum software
+ * Copyright (C) 2008-2012 FluxBB.org
+ * based on code by Rickard Andersson copyright (C) 2002-2008 PunBB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public license for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category	FluxBB
+ * @package		Core
+ * @copyright	Copyright (c) 2008-2012 FluxBB (http://fluxbb.org)
+ * @license		http://www.gnu.org/licenses/gpl.html	GNU General Public License
+ */
+
+class FluxBB_Update_Remove_Online
+{
+	
+	public function up()
+	{
+		Schema::drop('online');
+	}
+
+	public function down()
+	{
+		Schema::table('online', function($table)
+		{
+			$table->create();
+
+			$table->integer('user_id')->unsigned()->default(1);
+			$table->string('ident', 200)->default('');
+			$table->integer('logged')->unsigned()->default(0);
+			$table->boolean('idle')->default(false);
+			$table->integer('last_post')->unsigned()->nullable();
+			$table->integer('last_search')->unsigned()->nullable();
+
+			$table->unique(array('user_id', 'ident'));
+			$table->index('ident');
+			$table->index('logged');
+		});
+	}
+
+}
