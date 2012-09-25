07f2b63bd2e89e117f8801134e651cee8fb3b835
Franz Liedke <franz@develophp.org>
2012-09-25 15:24:01 +0200

    Fix namespace in RouteServiceProvider.

--- src/FluxBB/Core/Providers/RouteServiceProvider.php
+++ src/FluxBB/Core/Providers/RouteServiceProvider.php
@@ -23,7 +23,7 @@
  * @license		http://www.gnu.org/licenses/gpl.html	GNU General Public License
  */
 
-namespace FluxBB;
+namespace FluxBB\Core\Providers;
 
 use Illuminate\Foundation\Application,
 	Illuminate\Foundation\Providers\ServiceProvider;
