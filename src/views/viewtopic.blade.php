f4c2fb6b7816fd4d92bcce84a5546ed49ace92d4
Franz Liedke <franz@develophp.org>
2012-09-27 16:04:57 +0200

    Work on compatibility in viewtopic view.

--- src/views/viewtopic.blade.php
+++ src/views/viewtopic.blade.php
@@ -1,11 +1,11 @@
-@layout('fluxbb::layout.main')
+@extends('fluxbb::layout.main')
 
 @section('main')
 
 <div class="linkst">
 	<div class="inbox crumbsplus">
 		<div class="pagepost">
-			<p class="postlink conr"><a href="{{ URL::to_action('fluxbb::posting@reply', array($topic->id)) }}">{{ __('fluxbb::topic.post_reply') }}</a></p>
+			<p class="postlink conr"><a href="{{ URL::action('fluxbb::posting@reply', array($topic->id)) }}">{{ trans('fluxbb::topic.post_reply') }}</a></p>
 		</div>
 		<div class="clearer"></div>
 	</div>
@@ -24,14 +24,14 @@ if ($post_count == 1) $post_classes .= ' blockpost1';
 
 ?>
 <div id="p{{ $post->id }}" class="blockpost {{ $post_classes }}">
-	<h2><span><span class="conr">#{{ $start_from + $post_count }}</span> <a href="{{ URL::to_action('fluxbb::home@post', array($post->id)) }}#p{{ $post->id }}">{{ HTML::format_time($post->posted) }}</a></span></h2>
+	<h2><span><span class="conr">#{{ $start_from + $post_count }}</span> <a href="{{ URL::action('fluxbb::home@post', array($post->id)) }}#p{{ $post->id }}">{{ HTML::format_time($post->posted) }}</a></span></h2>
 	<div class="box">
 		<div class="inbox">
 			<div class="postbody">
 				<div class="postleft">
 					<dl>
-	@if (fluxbb\Models\User::current()->can_view_users())
-						<dt><strong>{{ HTML::link_to_action('fluxbb::user@profile', $post->poster->username, array($post->poster_id)) }}</strong></dt>
+	@if (fluxbb\Models\User::current()->canViewUsers())
+						<dt><strong><a href="{{ URL::action('fluxbb::user@profile', array($post->poster_id)) }}">{{ e($post->poster->username) }}</a></strong></dt>
 	@else
 						<dt><strong>{{ e($post->poster->username) }}</strong></dt><!-- TODO: linkify if logged in and g_view_users is enabled for this group -->
 	@endif
@@ -40,31 +40,31 @@ if ($post_count == 1) $post_classes .= ' blockpost1';
 						<dd class="postavatar">{{ HTML::avatar($post->poster) }}</dd>
 	@endif
 	@if ($post->poster->has_location()) <!-- TODO: and if user is allowed to view this (logged in and show_user_info -->
-						<dd><span>{{ __('fluxbb::topic.from', array('name' => e($post->poster->location))) }}</span></dd>
+						<dd><span>{{ trans('fluxbb::topic.from', array('name' => e($post->poster->location))) }}</span></dd>
 	@endif
-						<dd><span>{{ __('fluxbb::topic.registered', array('time' => HTML::format_time($post->poster->registered, true))) }}</span></dd>
-						<dd><span>{{ __('fluxbb::topic.posts', array('count' => HTML::number_format($post->poster->num_posts))) }}</span></dd>
-						<dd><span><a href="{{ URL::to_action('fluxbb::moderate@host', array($post->id)) }}" title="{{ $post->poster->ip }}">{{ __('fluxbb::topic.ip_address_logged') }}</a></span></dd>
+						<dd><span>{{ trans('fluxbb::topic.registered', array('time' => HTML::format_time($post->poster->registered, true))) }}</span></dd>
+						<dd><span>{{ trans('fluxbb::topic.posts', array('count' => HTML::number_format($post->poster->num_posts))) }}</span></dd>
+						<dd><span><a href="{{ URL::action('fluxbb::moderate@host', array($post->id)) }}" title="{{ $post->poster->ip }}">{{ trans('fluxbb::topic.ip_address_logged') }}</a></span></dd>
 	@if ($post->poster->has_admin_note())
-						<dd><span>{{ __('fluxbb::topic.note') }} <strong>{{ e($post->poster->admin_note) }}</strong></span></dd>
+						<dd><span>{{ trans('fluxbb::topic.note') }} <strong>{{ e($post->poster->admin_note) }}</strong></span></dd>
 	@endif
 
 						<dd class="usercontacts">
-							<span class="email"><a href="mailto:{{ $post->poster_email }}">{{ __('fluxbb::common.email') }}</a></span>
-							<span class="email"><a href="{{ URL::to_action('fluxbb::misc@email', array($post->poster_id)) }}">{{ __('fluxbb::common.email') }}</a></span>
+							<span class="email"><a href="mailto:{{ $post->poster_email }}">{{ trans('fluxbb::common.email') }}</a></span>
+							<span class="email"><a href="{{ URL::action('fluxbb::misc@email', array($post->poster_id)) }}">{{ trans('fluxbb::common.email') }}</a></span>
 	@if ($post->poster->has_url())
-							<span class="website"><a href="{{ e($post->poster->url) }}">{{ __('fluxbb::topic.website') }}</a></span>
+							<span class="website"><a href="{{ e($post->poster->url) }}">{{ trans('fluxbb::topic.website') }}</a></span>
 	@endif
 						</dd>
 
 					</dl>
 				</div>
 				<div class="postright">
-					<h3><?php if ($post->id != $topic->first_post_id) echo __('fluxbb::topic.re').' '; ?>{{ e($topic->subject) }}</h3>
+					<h3><?php if ($post->id != $topic->first_post_id) echo trans('fluxbb::topic.re').' '; ?>{{ e($topic->subject) }}</h3>
 					<div class="postmsg">
 						{{ $post->message() }}
 	@if ($post->was_edited())
-						<p class="postedit"><em>{{ __('fluxbb::topic.last_edit').' '.e($post->edited_by).' ('.HTML::format_time($post->edited) }})</em></p>
+						<p class="postedit"><em>{{ trans('fluxbb::topic.last_edit').' '.e($post->edited_by).' ('.HTML::format_time($post->edited) }})</em></p>
 	@endif
 					</div>
 	@if ($post->poster->has_signature())
@@ -78,9 +78,9 @@ if ($post_count == 1) $post_classes .= ' blockpost1';
 				<div class="postfootleft">
 	@if (!$post->poster->is_guest())
 		@if ($post->poster->is_online())
-					<p><strong>{{ __('fluxbb::topic.online') }}</strong></p>
+					<p><strong>{{ trans('fluxbb::topic.online') }}</strong></p>
 		@else
-					<p><span>{{ __('fluxbb::topic.offline') }}</span></p>
+					<p><span>{{ trans('fluxbb::topic.offline') }}</span></p>
 		@endif
 	@endif
 				</div>
@@ -88,10 +88,10 @@ if ($post_count == 1) $post_classes .= ' blockpost1';
 				<div class="postfootright">
 					<ul>
 						<!-- TODO: Only show these if appropriate -->
-						<li class="postreport"><span><a href="{{ URL::to_action('fluxbb::misc@report', array($post->id)) }}">{{ __('fluxbb::topic.report') }}</a></span></li>
-						<li class="postdelete"><span><a href="{{ URL::to_action('fluxbb::post@delete', array($post->id)) }}">{{ __('fluxbb::topic.delete') }}</a></span></li>
-						<li class="postedit"><span><a href="{{ URL::to_action('fluxbb::post@edit', array($post->id)) }}">{{ __('fluxbb::topic.edit') }}</a></span></li>
-						<li class="postquote"><span><a href="{{ URL::to_action('fluxbb::post@quote', array($topic->id, $post->id)) }}">{{ __('fluxbb::topic.quote') }}</a></span></li>
+						<li class="postreport"><span><a href="{{ URL::action('fluxbb::misc@report', array($post->id)) }}">{{ trans('fluxbb::topic.report') }}</a></span></li>
+						<li class="postdelete"><span><a href="{{ URL::action('fluxbb::post@delete', array($post->id)) }}">{{ trans('fluxbb::topic.delete') }}</a></span></li>
+						<li class="postedit"><span><a href="{{ URL::action('fluxbb::post@edit', array($post->id)) }}">{{ trans('fluxbb::topic.edit') }}</a></span></li>
+						<li class="postquote"><span><a href="{{ URL::action('fluxbb::post@quote', array($topic->id, $post->id)) }}">{{ trans('fluxbb::topic.quote') }}</a></span></li>
 					</ul>
 				</div>
 	@endif
@@ -104,7 +104,7 @@ if ($post_count == 1) $post_classes .= ' blockpost1';
 <div class="postlinksb">
 	<div class="inbox crumbsplus">
 		<div class="pagepost">
-			<p class="postlink conr"><a href="{{ URL::to_action('fluxbb::posting@reply', array($topic->id)) }}">{{ __('fluxbb::topic.post_reply') }}</a></p>
+			<p class="postlink conr"><a href="{{ URL::action('fluxbb::posting@reply', array($topic->id)) }}">{{ trans('fluxbb::topic.post_reply') }}</a></p>
 		</div>
 		<div class="clearer"></div>
 	</div>
