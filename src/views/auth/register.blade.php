c0bf6f4573d5a41006425f7968912ab827c006e4
Franz Liedke <franz@develophp.org>
2012-09-30 00:03:26 +0200

    Update the registration form view.

--- src/views/auth/register.blade.php
+++ src/views/auth/register.blade.php
@@ -1,72 +1,72 @@
-@layout('fluxbb::layout.main')
+@extends('fluxbb::layout.main')
 
 @section('main')
 <div id="regform" class="blockform">
-	<h2><span>{{ __('fluxbb::register.register') }}</span></h2>
+	<h2><span>{{ trans('fluxbb::register.register') }}</span></h2>
 	<div class="box">
-		<form id="register" method="post" action="{{ URL::to_action('fluxbb::auth@register') }}" onsubmit="this.register.disabled=true;if(process_form(this)){return true;}else{this.register.disabled=false;return false;}">
+		<form id="register" method="post" action="{{ URL::action('fluxbb::auth@register') }}" onsubmit="this.register.disabled=true;if(process_form(this)){return true;}else{this.register.disabled=false;return false;}">
 			<div class="inform">
 				<div class="forminfo">
-					<h3>{{ __('fluxbb::common.important') }}</h3>
-					<p>{{ __('fluxbb::register.desc1') }}</p>
-					<p>{{ __('fluxbb::register.desc2') }}</p>
+					<h3>{{ trans('fluxbb::common.important') }}</h3>
+					<p>{{ trans('fluxbb::register.desc1') }}</p>
+					<p>{{ trans('fluxbb::register.desc2') }}</p>
 				</div>
 				<fieldset>
-					<legend>{{ __('fluxbb::register.legend_username') }}</legend>
+					<legend>{{ trans('fluxbb::register.legend_username') }}</legend>
 					<div class="infldset">
 						<input type="hidden" name="form_sent" value="1" />
 						{{-- TODO: Repopulate this with old values on errors --}}
 						<label class="required">
-							<strong>{{ __('fluxbb::common.username') }} <span>{{ __('fluxbb::common.required') }}</span></strong><br />
-							{{ Form::text('user', Input::old('user'), array('size' => 25, 'maxlength' => 25)) }}<br />
+							<strong>{{ trans('fluxbb::common.username') }} <span>{{ trans('fluxbb::common.required') }}</span></strong><br />
+							<input type="text" name="user" size="25" maxlength="25" value="{{ Input::old('user') }}" /><br />
 						</label>
 					</div>
 				</fieldset>
 			</div>
-@if (fluxbb\Models\Config::disabled('o_regs_verify'))
+@if (FluxBB\Models\Config::disabled('o_regs_verify'))
 			<div class="inform">
 				<fieldset>
-					<legend>{{ __('fluxbb::register.legend_pass') }}</legend>
+					<legend>{{ trans('fluxbb::register.legend_pass') }}</legend>
 					<div class="infldset">
 						<label class="conl required">
-							<strong>{{ __('fluxbb::common.password') }} <span>{{ __('fluxbb::common.required') }}</span></strong><br />
-							{{ Form::password('password', array('size' => 16)) }}<br />
+							<strong>{{ trans('fluxbb::common.password') }} <span>{{ trans('fluxbb::common.required') }}</span></strong><br />
+							<input type="password" name="password" size="16" /><br />
 						</label>
 						<label class="conl required">
-							<strong>{{ __('fluxbb::prof_reg.confirm_pass') }} <span>{{ __('fluxbb::common.required') }}</span></strong><br />
-							{{ Form::password('password_confirmation', array('size' => 16)) }}<br />
+							<strong>{{ trans('fluxbb::prof_reg.confirm_pass') }} <span>{{ trans('fluxbb::common.required') }}</span></strong><br />
+							<input type="password" name="password_confirmation" size="16" /><br />
 						</label>
-						<p class="clearb">{{ __('fluxbb::register.info_pass') }}</p>
+						<p class="clearb">{{ trans('fluxbb::register.info_pass') }}</p>
 					</div>
 				</fieldset>
 			</div>
 @endif
 			<div class="inform">
 				<fieldset>
-@if (fluxbb\Models\Config::enabled('o_regs_verify'))
-					<legend>{{ __('fluxbb::prof_reg.legend_email2') }}</legend>
+@if (FluxBB\Models\Config::enabled('o_regs_verify'))
+					<legend>{{ trans('fluxbb::prof_reg.legend_email2') }}</legend>
 @else
-					<legend>{{ __('fluxbb::prof_reg.legend_email') }}</legend>
+					<legend>{{ trans('fluxbb::prof_reg.legend_email') }}</legend>
 @endif
 					<div class="infldset">
-@if (fluxbb\Models\Config::enabled('o_regs_verify'))
-					<p>{{ __('fluxbb::register.info_email') }}</p>
+@if (FluxBB\Models\Config::enabled('o_regs_verify'))
+					<p>{{ trans('fluxbb::register.info_email') }}</p>
 @endif
 						<label class="required">
-							<strong>{{ __('fluxbb::common.email') }} <span>{{ __('fluxbb::common.required') }}</span></strong><br />
-							{{ Form::text('email', Input::old('email'), array('size' => 50, 'maxlength' => 80)) }}<br />
+							<strong>{{ trans('fluxbb::common.email') }} <span>{{ trans('fluxbb::common.required') }}</span></strong><br />
+							<input type="email" name="email" size="50" maxlength="80" value="{{ Input::old('email') }}" /><br />{{-- TODO: Escape old input (see above, too) --}}
 						</label>
-@if (fluxbb\Models\Config::enabled('o_regs_verify'))
+@if (FluxBB\Models\Config::enabled('o_regs_verify'))
 						<label class="required">
-							<strong>{{ __('fluxbb::register.confirm_email') }} <span>{{ __('fluxbb::common.required') }}</span></strong><br />
-							{{ Form::text('email_confirmation', Input::old('email_confirmation'), array('size' => 50, 'maxlength' => 80)) }}<br />
+							<strong>{{ trans('fluxbb::register.confirm_email') }} <span>{{ trans('fluxbb::common.required') }}</span></strong><br />
+							<input type="email" name="email_confirmation" size="50" maxlength="80" value="{{ Input::old('email_confirmation') }}" /><br />
 						</label>
 @endif
 					</div>
 				</fieldset>
 			</div>
-			<p class="buttons"><input type="submit" name="register" value="{{ __('fluxbb::register.register') }}" /></p>
+			<p class="buttons"><input type="submit" name="register" value="{{ trans('fluxbb::register.register') }}" /></p>
 		</form>
 	</div>
 </div>
-@endsection
+@stop
