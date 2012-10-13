e2cf2a94b72bf6d6b9e0db2d8ffdfe2eacc191dc
Franz Liedke <franz@develophp.org>
2012-10-14 00:10:26 +0200

    Add table name to bans model.

--- src/FluxBB/Models/Ban.php
+++ src/FluxBB/Models/Ban.php
@@ -28,6 +28,9 @@ namespace FluxBB\Models;
 class Ban extends Base
 {
 
+	protected $table = 'bans';
+
+
 	public function creator()
 	{
 		return $this->belongsTo('FluxBB\\Models\\User', 'ban_creator');
