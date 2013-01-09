072888ac3b1701e4d7c7466b74888e665f9e9663
Franz Liedke <franz@develophp.org>
2013-01-09 12:11:37 +0100

    Update admin menu.

--- views/admin/layout/menu.blade.php
+++ views/admin/layout/menu.blade.php
@@ -2,7 +2,8 @@
 	<ul>
 		<li class="active"><a href="#">Dashboard</a></li>
 		<li><a href="#">Content</a></li>
-		<li><a href="#">Moderation</a></li>
 		<li><a href="#">Users</a></li>
+		<li><a href="#">Settings</a></li>
+		<li><a href="#">Extensions</a></li>
 	</ul>
 </nav>
