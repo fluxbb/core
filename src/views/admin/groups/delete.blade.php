f7d79dda4d366e1a6bcd99de41882809df61d550
Franz Liedke <franz@develophp.org>
2013-02-27 20:36:33 +0100

    #814: Add group deletion confirmation view.

---
+++ src/views/admin/groups/delete.blade.php
@@ -0,0 +1,12 @@
+@extends('fluxbb::admin.layout.main')
+
+@section('main')
+
+Do you really want to delete the group "{{ $group->title }}"?
+
+<form method="POST">
+	<input type="submit" value="Yes!" />
+	<a href="{{ route('admin_groups_index') }}">No, take me back!</a>
+</form>
+
+@stop
