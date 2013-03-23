526a5e84a5d8d244c07bddacf484248f9c70718d
Franz Liedke <franz@develophp.org>
2013-03-23 03:19:56 +0100

    Clean up admin menu.

--- src/views/admin/layout/menu.blade.php
+++ src/views/admin/layout/menu.blade.php
@@ -1,4 +1,4 @@
-<h6>Dashboard</h6>
+
 <ul class="nav">
 	<li><a href="{{ route('admin') }}">Dashboard</a></li>
 	<li><a href="#">Updates</a></li>
@@ -6,20 +6,17 @@
 <h6>Content</h6>
 <ul class="nav">
 	<li><a href="#">Forums</a></li>
-	<li><a href="#">Markup manager</a></li>
-	<li><a href="#">Censoring</a></li>
 	<li><a href="#">Spam and reports</a></li>
 </ul>
 <h6>Users</h6>
 <ul class="nav">
 	<li><a href="#">User manager</a></li>
 	<li><a href="{{ route('admin_groups_index') }}">Groups</a></li>
-	<li><a href="#">Permissions</a></li>
 	<li><a href="#">Bans</a></li>
 </ul>
 <h6>Settings</h6>
 <ul class="nav">
-	<li><a href="#">Global</a></li>
+	<li><a href="{{ route('admin_settings_global') }}">Global</a></li>
 	<li><a href="#">Email</a></li>
 	<li><a href="#">Other</a></li>
 	<li><a href="#">Maintenance and backup</a></li>
