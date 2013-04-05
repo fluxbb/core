d17aebd72465bf23eceb9b614dd8461e721f9977
Franz Liedke <franz@develophp.org>
2013-04-05 18:51:49 +0200

    Add most basic settings to global settings page.

--- src/views/admin/settings/global.blade.php
+++ src/views/admin/settings/global.blade.php
@@ -4,9 +4,31 @@
 
 <h2>Global settings</h2>
 
-<div class="setting" data-key="description">
+<div class="setting" data-key="board_title">
+	<label>Board title</label>
+	<input type="text" name="board_title" />
+</div>
+
+<div class="setting" data-key="board_description">
 	<label>Description</label>
-	<input type="text" name="description" />
+	<input type="text" name="board_description" />
+</div>
+
+<div class="setting">
+	<label>Default language</label>
+	<select name="default_language">
+		<option value="en">English</option>
+	</select>
+</div>
+
+<div class="setting">
+	<label>Admin email</label>
+	<input type="email" name="admin_email" />
+</div>
+
+<div class="setting">
+	<label>Webmaster email</label>
+	<input type="email" name="webmaster_email" />
 </div>
 
 @stop
