c3b71a8103f7edbaaced57810f590a4083128819
Franz Liedke <franz@develophp.org>
2014-01-29 11:29:17 +0100

    Add some validation helpers to base model class.

--- src/FluxBB/Models/Base.php
+++ src/FluxBB/Models/Base.php
@@ -3,10 +3,34 @@
 namespace FluxBB\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Validator;
 
 class Base extends Model
 {
 
 	public $timestamps = false;
+
+	protected $rules = array();
+
+	protected $errors = array();
+
+
+	public function valid()
+	{
+		if (empty($this->rules)) return true;
+
+		$v = Validator::make($this->attributes, $this->rules);
+		return $v->passes();
+	}
+
+	public function invalid()
+	{
+		return ! $this->valid();
+	}
+
+	public function getErrors()
+	{
+		return $this->errors;
+	}
 
 }
