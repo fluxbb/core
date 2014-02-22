c6358375544abe0ec1ff849a8110dff741f785b7
Franz Liedke <franz@develophp.org>
2014-02-22 02:00:11 +0100

    Implement some shortcuts for throwing standard HTTP exceptions in controllers.

--- src/FluxBB/Controllers/BaseController.php
+++ src/FluxBB/Controllers/BaseController.php
@@ -5,6 +5,9 @@ namespace FluxBB\Controllers;
 use FluxBB\Models\User;
 use Illuminate\Routing\Controller;
 use Illuminate\Validation\Validator;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class BaseController extends Controller
 {
@@ -20,5 +23,20 @@ class BaseController extends Controller
     public function validator($attributes, $rules, $messages = array())
     {
         return \Validator::make($attributes, $rules, $messages);
+    }
+
+    public function notFound()
+    {
+        throw new NotFoundHttpException;
+    }
+
+    public function badRequest()
+    {
+        throw new BadRequestHttpException;
+    }
+
+    public function forbidden()
+    {
+        throw new AccessDeniedHttpException;
     }
 }
