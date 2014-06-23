4507827dd21159aa1ec24582f4f1ed894c5ed927
Franz Liedke <franz@develophp.org>
2014-06-23 17:54:09 +0200

    #741: Improve 404 page.

--- src/views/error/404.blade.php
+++ src/views/error/404.blade.php
@@ -1,2 +1,11 @@
-<h2>Not Found</h2>
-<p>Nothing to see here. Move along.</p>
+@extends('fluxbb::layout.main')
+
+@section('main')
+
+<div class="error" id="error-404">
+	<h2>Not Found</h2>
+	<p>Nothing to see here. Move along.</p>
+</div>
+
+
+@stop
