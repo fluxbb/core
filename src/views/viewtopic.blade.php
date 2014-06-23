33f5f3263bddeba95668344ed06e5b713563d15c
Franz Liedke <franz@develophp.org>
2014-06-23 16:11:49 +0200

    Wrap quickpost fields in a form.

--- src/views/viewtopic.blade.php
+++ src/views/viewtopic.blade.php
@@ -96,23 +96,25 @@
 </div>
 
 <div id="quickpost" class="post post-bg clearfix">
-    <div class="author-box col-md-2 col-sm-2 col-xs-2">
-        <div class="author-name"><h4>Quick reply</h4></div>
-    </div>
-    <div class="post-box col-md-10 col-sm-10 col-xs-10">
-        <div class="post-content">
-            <textarea class="new-message" rows="7" cols="75" placeholder="Write your message and submit!"></textarea>
+    <form action="{{ route('reply', array('id' => $topic->id)) }}" method="POST">
+        <div class="author-box col-md-2 col-sm-2 col-xs-2">
+            <div class="author-name"><h4>Quick reply</h4></div>
         </div>
-    </div>
-    <div class="new-post-submit col-md-offset-2">
-        <div class="post-options pull-left">
-            <span class="label label-success">BBCode</a></span>
-            <span class="label label-success">[url] tag</a></span>
-            <span class="label label-danger">[img] tag</a></span>
-            <span class="label label-success">Smilies</a></span>
+        <div class="post-box col-md-10 col-sm-10 col-xs-10">
+            <div class="post-content">
+                <textarea name="req_message" class="new-message" rows="7" cols="75" placeholder="Write your message and submit!"></textarea>
+            </div>
         </div>
-        <input class="btn btn-success pull-right" type="submit" value="Submit" />
-    </p>
+        <div class="new-post-submit col-md-offset-2">
+            <div class="post-options pull-left">
+                <span class="label label-success">BBCode</a></span>
+                <span class="label label-success">[url] tag</a></span>
+                <span class="label label-danger">[img] tag</a></span>
+                <span class="label label-success">Smilies</a></span>
+            </div>
+            <input class="btn btn-success pull-right" type="submit" value="Submit" />
+        </p>
+    </form>
 </div>
 
 @stop
