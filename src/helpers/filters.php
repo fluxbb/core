92af82dcabb2d06d0cbb9f21e4d3ec85bd9f31bc
Franz Liedke <franz@develophp.org>
2014-07-30 00:05:47 +0200

    Remove unused filters.

--- src/helpers/filters.php
+++ src/helpers/filters.php
@@ -6,17 +6,3 @@ Route::filter('fluxbb_is_installed', function () {
             ->with('has_installer', false);
     }
 });
-
-Route::filter('only_guests', function () {
-    if (!Auth::guest()) {
-        return Redirect::to_action('home@index');
-    }
-});
-
-Route::filter('only_members', function () {
-    if (!Auth::check()) {
-        return Redirect::to_action('auth@login')
-            ->with('message', trans('fluxbb::common.login_to_view'))
-            ->with('login_redirect', URL::current()); // TODO: URL::current() is not yet implemented
-    }
-});
