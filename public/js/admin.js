00032298054b150442d7a7064bbe5c3d14b6824c
Franz Liedke <franz@develophp.org>
2014-09-11 10:43:45 +0200

    Clean up old admin JavaScript.

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,30 +1,4 @@
-$(document).ready(function() {
-	$('#board-info p').click(function() {
-		$(this).parent().addClass('edit');
-		$('#board-info p').each(function(index, element) {
-			$(element).next().val(element.innerHTML);
-		});
-		$(this).next().focus();
-	});
-
-	$('#board-info input.submit').click(function() {
-		$('#board-info p').each(function(index, element) {
-			element.innerHTML = $(element).next().val();
-		});
-		$(this).parent().removeClass('edit');
-
-		$.ajax({
-			type: "POST",
-			url: baseUrl + "/admin/ajax/board_config",
-			data: {
-				board_title: $('#board-info input.title').val(),
-				board_description: $('#board-info input.description').val()
-			}
-		}).done(function(data) {
-			alert('saved');
-		});
-	});
-
+jQuery(function($) {
 	$(document).on('change', '.js-save-on-change', function() {
         var $field = $(this);
         var name = $field.attr('name');
