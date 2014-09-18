dcc5e3a441ad69513cb05781f849db7454acdbab
Franz Liedke <franz@develophp.org>
2014-09-19 01:48:42 +0200

    Add back format_time() helper for now.

--- src/FluxBB/Web/ServiceProvider.php
+++ src/FluxBB/Web/ServiceProvider.php
@@ -68,6 +68,44 @@ class ServiceProvider extends Base
             $view->share('method', function ($name) use ($app) {
                 return $app['fluxbb.web.router']->getMethod($name);
             });
+
+            $view->share('format_time', function ($timestamp, $date_only = false, $date_format = null, $time_format = null, $time_only = false, $no_text = false) {
+                if ($timestamp == '') {
+                    return trans('fluxbb::common.never');
+                }
+
+                $diff = (0 + 0) * 3600; // FIXME: $pun_user['timezone'] + $pun_user['dst'];
+                $timestamp += $diff;
+                $now = time();
+
+                if (is_null($date_format)) {
+                    $date_format = 'Y-m-d'; // FIXME: $forum_date_formats[$pun_user['date_format']];
+                }
+
+                if (is_null($time_format)) {
+                    $time_format = 'H:i'; // FIXME: $forum_time_formats[$pun_user['time_format']];
+                }
+
+                $date = gmdate($date_format, $timestamp);
+                $today = gmdate($date_format, $now + $diff);
+                $yesterday = gmdate($date_format, $now + $diff - 86400);
+
+                if (!$no_text) {
+                    if ($date == $today) {
+                        $date = trans('fluxbb::common.today');
+                    } elseif ($date == $yesterday) {
+                        $date = trans('fluxbb::common.yesterday');
+                    }
+                }
+
+                if ($date_only) {
+                    return $date;
+                } elseif ($time_only) {
+                    return gmdate($time_format, $timestamp);
+                } else {
+                    return $date.' '.gmdate($time_format, $timestamp);
+                }
+            });
         });
     }
 
--- src/views/user/list.blade.php
+++ src/views/user/list.blade.php
@@ -18,9 +18,9 @@ Pages: {{{ $users->links() }}}
 @foreach ($users as $user)
         <tr>
             <td><a href="{{ $route('profile', array('id' => $user->id)) }}">{{ $user->username }}</a></td>
-            <td>{{$user->title}}</td>
-            <td>{{$user->num_posts}}</td>
-            <td>{{ HTML::format_time($user->registered, true) }}</td>
+            <td>{{ $user->title }}</td>
+            <td>{{ $user->num_posts }}</td>
+            <td>{{ $format_time($user->registered, true) }}</td>
         </tr>
 @endforeach
 
