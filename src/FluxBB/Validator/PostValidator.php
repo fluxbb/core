41dbaafe4dcabdfd851c0397efe937813af13ded
Franz Liedke <franz@develophp.org>
2014-09-19 00:44:17 +0200

    Add docblocks.

--- src/FluxBB/Validator/PostValidator.php
+++ src/FluxBB/Validator/PostValidator.php
@@ -7,11 +7,22 @@ use FluxBB\Models\Post;
 
 class PostValidator extends Validator
 {
+    /**
+     * The rules to validate against.
+     *
+     * @var array
+     */
     protected $rules = [
         'message' => 'required',
     ];
 
 
+    /**
+     * Make sure the given post is valid.
+     *
+     * @param \FluxBB\Models\Post $post
+     * @throws \FluxBB\Server\Exception\ValidationFailed
+     */
     public function validate(Post $post)
     {
         $this->ensureValid($post->getAttributes());
