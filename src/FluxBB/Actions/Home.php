92546fa3112f4706b47c85f257be1408f4463236
Franz Liedke <franz@develophp.org>
2014-09-22 16:26:44 +0200

    Make the home page redirect to its category URL. (reverted from commit c90b260bacd480cde189033eeee2f3909546cb8f)

--- src/FluxBB/Actions/Home.php
+++ src/FluxBB/Actions/Home.php
@@ -3,14 +3,29 @@
 namespace FluxBB\Actions;
 
 use FluxBB\Core\Action;
-use FluxBB\Server\Request;
+use FluxBB\Models\CategoryRepositoryInterface;
 
 class Home extends Action
 {
+    /**
+     * The category repository instance.
+     *
+     * @var \FluxBB\Models\CategoryRepositoryInterface
+     */
+    protected $categories;
+
+
+    public function __construct(CategoryRepositoryInterface $repository)
+    {
+        $this->categories = $repository;
+    }
+
     protected function run()
     {
-        $this->redirectTo(
-            new Request('category', ['slug' => '/'])
-        );
+        $category = $this->categories->findBySlug('/');
+
+        $this->data['category'] = $category;
+        $this->data['categories'] = $this->categories->getByParent('/');
+        $this->data['conversations'] = $this->categories->getConversationsIn($category);
     }
 }
