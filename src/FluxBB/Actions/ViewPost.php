e20200efefd903408e2965226a03ae5e33f30816
Sxderp <googoo1337@yahoo.com>
2014-09-30 14:54:26 -0500

    Update ViewPost.php

--- src/FluxBB/Actions/ViewPost.php
+++ src/FluxBB/Actions/ViewPost.php
@@ -3,30 +3,30 @@
 namespace FluxBB\Actions;
 
 use FluxBB\Core\Action;
-use FluxBB\Models\ConversationRepositoryInterface;
+use FluxBB\Models\TopicRepositoryInterface;
 use FluxBB\Models\User;
 use FluxBB\Server\Request;
 
 class ViewPost extends Action
 {
-    protected $conversations;
+    protected $topics;
 
 
-    public function __construct(ConversationRepositoryInterface $repository)
+    public function __construct(TopicRepositoryInterface $repository)
     {
-        $this->conversations = $repository;
+        $this->topics = $repository;
     }
 
     protected function run()
     {
         $id = $this->request->get('id');
 
-        $post = $this->conversations->findPostById($id);
-        $page = $this->conversations->getPageOfPost($post, User::current()->dispPosts());
+        $post = $this->topics->findPostById($id);
+        $page = $this->topics->getPageOfPost($post, User::current()->dispPosts());
 
         $this->forwardTo(
-            new Request('conversation', [
-                'id' => $post->conversation_id,
+            new Request('topic', [
+                'id' => $post->topic_id,
                 'page' => $page,
             ])
         ); // TODO: Append #p to URL
