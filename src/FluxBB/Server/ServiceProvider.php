73b2d3119b9b5a7838fd578002430f88d4721f37
Sxderp <googoo1337@yahoo.com>
2014-09-30 14:59:23 -0500

    Update ServiceProvider.php

--- src/FluxBB/Server/ServiceProvider.php
+++ src/FluxBB/Server/ServiceProvider.php
@@ -47,7 +47,7 @@ class ServiceProvider extends Base
 
         $server->register('index', 'FluxBB\Actions\Home');
         $server->register('category', 'FluxBB\Actions\ViewCategory');
-        $server->register('conversation', 'FluxBB\Actions\ViewConversation');
+        $server->register('topic', 'FluxBB\Actions\ViewTopic');
         $server->register('viewpost', 'FluxBB\Actions\ViewPost');
         $server->register('register', 'FluxBB\Actions\RegisterPage');
         $server->register('handle_registration', 'FluxBB\Actions\Register');
