c2ff770fe1b8d1180422dfdde70dd06d9904f01c
Franz Liedke <franz@develophp.org>
2014-10-15 01:29:05 +0200

    Add one more test to make sure input can be accessed.

--- spec/FluxBB/Core/ActionSpec.php
+++ spec/FluxBB/Core/ActionSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\FluxBB\Core;
 
+use FluxBB\Server\Response;
 use Illuminate\Contracts\Events\Dispatcher;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -9,6 +10,20 @@ use FluxBB\Core\Action;
 
 class ActionSpec extends ObjectBehavior
 {
+    function it_always_returns_responses()
+    {
+        $this->beAnInstanceOf('spec\FluxBB\Core\EmptyAction');
+
+        $this->execute()->shouldReturnAnInstanceOf('FluxBB\Server\Response');
+    }
+
+    function it_can_access_input()
+    {
+        $this->beAnInstanceOf('spec\FluxBB\Core\InputAction');
+
+        $this->execute(['foo' => 'bar', 'baz' => 'bam'])->getData()->shouldReturn(['result' => 'barbam']);
+    }
+
     function it_can_raise_events(Dispatcher $dispatcher)
     {
         $this->beAnInstanceOf('spec\FluxBB\Core\EventAction');
@@ -18,19 +33,23 @@ class ActionSpec extends ObjectBehavior
         $this->setEvents($dispatcher);
         $this->execute();
     }
-
-    function it_always_returns_responses()
-    {
-        $this->beAnInstanceOf('spec\FluxBB\Core\EmptyAction');
-
-        $this->execute()->shouldReturnAnInstanceOf('FluxBB\Server\Response');
-    }
 }
 
 class EmptyAction extends Action
 {
     protected function run()
     { }
+}
+
+class InputAction extends Action
+{
+    protected function run()
+    {
+        $foo = $this->get('foo');
+        $baz = $this->get('baz');
+        $result = $foo.$baz;
+        return compact('result');
+    }
 }
 
 class EventAction extends Action
