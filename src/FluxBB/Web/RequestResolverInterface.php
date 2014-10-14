a254dcf098ad657d3117f7f3cfb382e107f015ab
Franz Liedke <franz@develophp.org>
2014-10-15 00:31:46 +0200

    Add an interface for resolving internal requests, create a Symfony implementation.

---
+++ src/FluxBB/Web/RequestResolverInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace FluxBB\Web;
+
+interface RequestResolverInterface
+{
+    /**
+     * Get the HTTP method of the request.
+     *
+     * @return string
+     */
+    public function getMethod();
+
+    /**
+     * Get the URI path we are requesting.
+     *
+     * @return string
+     */
+    public function getUri();
+
+    /**
+     * Get any parameters sent along with the request.
+     *
+     * @return array
+     */
+    public function getParameters();
+}
---
+++ src/FluxBB/Web/SymfonyRequestResolver.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace FluxBB\Web;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class SymfonyRequestResolver implements RequestResolverInterface
+{
+    /**
+     * @var \Symfony\Component\HttpFoundation\Request
+     */
+    protected $request;
+
+
+    /**
+     * Create the resolver instance.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Get the HTTP method of the request.
+     *
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->request->getMethod();
+    }
+
+    /**
+     * Get the URI path we are requesting.
+     *
+     * @return string
+     */
+    public function getUri()
+    {
+        return $this->request->getPathInfo();
+    }
+
+    /**
+     * Get any parameters sent along with the request.
+     *
+     * @return array
+     */
+    public function getParameters()
+    {
+        return $this->request->query->all() + $this->request->attributes->all();
+    }
+
+}
