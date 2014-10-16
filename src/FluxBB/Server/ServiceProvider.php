2da0fa5f47d4b0d0cd19a44582cea7d8f88b6302
Franz Liedke <franz@develophp.org>
2014-10-16 10:48:34 +0200

    Unregister actions that aren't really actions.

--- src/FluxBB/Server/ServiceProvider.php
+++ src/FluxBB/Server/ServiceProvider.php
@@ -58,32 +58,17 @@ class ServiceProvider extends Base
         $server->registerAction('category', 'FluxBB\Actions\ViewCategory');
         $server->registerAction('conversation', 'FluxBB\Actions\ViewConversation');
         $server->registerAction('viewpost', 'FluxBB\Actions\ViewPost');
-        $server->registerAction('register', 'FluxBB\Actions\RegisterPage');
         $server->registerAction('handle_registration', 'FluxBB\Actions\Register');
-        $server->registerAction('login', 'FluxBB\Actions\LoginPage');
         $server->registerAction('handle_login', 'FluxBB\Actions\Login');
         $server->registerAction('logout', 'FluxBB\Actions\Logout');
-        $server->registerAction('reset_password', 'FluxBB\Actions\PasswordResetPage');
-        $server->registerAction('profile', 'FluxBB\Actions\ProfilePage');
-        $server->registerAction('userlist', 'FluxBB\Actions\UsersPage');
         $server->registerAction('rules', 'FluxBB\Actions\Rules');
         $server->registerAction('search', 'FluxBB\Actions\SearchPage');
-        $server->registerAction('post_edit', 'FluxBB\Actions\EditPostPage');
         $server->registerAction('post_edit_handler', 'FluxBB\Actions\EditPost');
         $server->registerAction('reply_handler', 'FluxBB\Actions\Reply');
         $server->registerAction('topic_subscribe', 'FluxBB\Actions\SubscribeTopic');
         $server->registerAction('topic_unsubscribe', 'FluxBB\Actions\UnsubscribeTopic');
-        $server->registerAction('new_topic', 'FluxBB\Actions\NewTopicPage');
         $server->registerAction('new_topic_handler', 'FluxBB\Actions\NewTopic');
-        $server->registerAction('admin.index', 'FluxBB\Actions\Admin\DashboardPage');
-        $server->registerAction('admin.settings.global', 'FluxBB\Actions\Admin\GlobalSettingsPage');
-        $server->registerAction('admin.settings.email', 'FluxBB\Actions\Admin\EmailSettingsPage');
-        $server->registerAction('admin.settings.maintenance', 'FluxBB\Actions\Admin\MaintenanceSettingsPage');
-        $server->registerAction('admin.dashboard.updates', 'FluxBB\Actions\Admin\UpdatesPage');
-        $server->registerAction('admin.dashboard.stats', 'FluxBB\Actions\Admin\StatsPage');
-        $server->registerAction('admin.dashboard.reports', 'FluxBB\Actions\Admin\ReportsPage');
         $server->registerAction('admin.options.set', 'FluxBB\Actions\Admin\SetOptions');
-        $server->registerAction('admin.categories.index', 'FluxBB\Actions\Admin\CategoriesList');
     }
 
     /**
