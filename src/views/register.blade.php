ad154f2956e10e922024cc6e9e88133fff19b35a
Franz Liedke <franz@develophp.org>
2014-10-26 22:52:01 +0100

    Clean up registration view.

--- src/views/register.blade.php
+++ src/views/register.blade.php
@@ -2,14 +2,9 @@
 
 @section('main')
 <h2>{{ trans('fluxbb::register.register') }}</h2>
-<form id="register" method="post" action="{{ $route('register') }}" onsubmit="this.register.disabled=true;if(process_form(this)){return true;}else{this.register.disabled=false;return false;}">
-    <h3>{{ trans('fluxbb::common.important') }}</h3>
-    <p>{{ trans('fluxbb::register.desc1') }}</p>
-    <p>{{ trans('fluxbb::register.desc2') }}</p>
-
+<form id="register" method="post" action="{{ $route('register') }}">
     <fieldset>
         <legend>{{ trans('fluxbb::register.legend_username') }}</legend>
-        {{-- TODO: Repopulate this with old values on errors --}}
         <label class="required">
             <strong>{{ trans('fluxbb::common.username') }} <span>{{ trans('fluxbb::common.required') }}</span></strong><br />
             <input type="text" name="username" size="25" maxlength="25" /><br />
@@ -29,10 +24,8 @@
         <p class="clearb">{{ trans('fluxbb::register.info_pass') }}</p>
     </fieldset>
     <fieldset>
-        <legend>{{ trans('fluxbb::prof_reg.legend_email2') }}</legend>
         <legend>{{ trans('fluxbb::prof_reg.legend_email') }}</legend>
 
-        <p>{{ trans('fluxbb::register.info_email') }}</p>
         <label class="required">
             <strong>{{ trans('fluxbb::common.email') }} <span>{{ trans('fluxbb::common.required') }}</span></strong><br />
             <input type="email" name="email" size="50" maxlength="80" /><br />{{-- TODO: Escape old input (see above, too) --}}
