f8ef3a99e809d962744605329a62dc2e16b78d8e
Franz Liedke <franz@develophp.org>
2014-10-26 22:36:20 +0100

    No option switches in registration view.

--- src/views/register.blade.php
+++ src/views/register.blade.php
@@ -16,7 +16,6 @@
         </label>
     </fieldset>
 
-@if (FluxBB\Models\Config::disabled('o_regs_verify'))
     <fieldset>
         <legend>{{ trans('fluxbb::register.legend_pass') }}</legend>
         <label class="conl required">
@@ -29,38 +28,21 @@
         </label>
         <p class="clearb">{{ trans('fluxbb::register.info_pass') }}</p>
     </fieldset>
-@endif
     <fieldset>
-@if (FluxBB\Models\Config::enabled('o_regs_verify'))
         <legend>{{ trans('fluxbb::prof_reg.legend_email2') }}</legend>
-@else
         <legend>{{ trans('fluxbb::prof_reg.legend_email') }}</legend>
-@endif
 
-@if (FluxBB\Models\Config::enabled('o_regs_verify'))
         <p>{{ trans('fluxbb::register.info_email') }}</p>
-@endif
         <label class="required">
             <strong>{{ trans('fluxbb::common.email') }} <span>{{ trans('fluxbb::common.required') }}</span></strong><br />
             <input type="email" name="email" size="50" maxlength="80" /><br />{{-- TODO: Escape old input (see above, too) --}}
         </label>
-@if (FluxBB\Models\Config::enabled('o_regs_verify'))
         <label class="required">
             <strong>{{ trans('fluxbb::register.confirm_email') }} <span>{{ trans('fluxbb::common.required') }}</span></strong><br />
             <input type="email" name="email_confirmation" size="50" maxlength="80" /><br />
         </label>
-@endif
     </fieldset>
 
-@if (FluxBB\Models\Config::enabled('o_rules'))
-    <fieldset>
-        <legend>{{ trans('fluxbb::register.legend_pass') }}</legend>
-        <label class="required">
-            <strong>{{ trans('fluxbb::register.rules_legend') }} </strong><br /><br />{{ FluxBB\Models\Config::get('o_rules_message') }}
-            <p class="checkbox"><input type="checkbox" name="rules" value="1" />{{ trans('fluxbb::register.agree') }}<span><strong>{{ trans('fluxbb::common.required') }}</strong></p></span>
-        </label>
-    </fieldset>
-@endif
     <p class="buttons"><input type="submit" name="register" value="{{ trans('fluxbb::register.register') }}" /></p>
 </form>
 @stop
