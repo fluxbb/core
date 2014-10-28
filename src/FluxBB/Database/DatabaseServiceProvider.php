ce773aca806c6bde240242f166a4868bb9a01f91
Franz Liedke <franz@develophp.org>
2014-10-28 23:34:12 +0100

    New very simple database service provider.

---
+++ src/FluxBB/Database/DatabaseServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace FluxBB\Database;
+
+use Illuminate\Database\Connectors\ConnectionFactory;
+use Illuminate\Support\ServiceProvider;
+
+class DatabaseServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('Illuminate\Database\ConnectionInterface', function () {
+            $factory = new ConnectionFactory($this->app);
+
+            $connection = $factory->make($this->app['config']->get('fluxbb.database'));
+            $connection->setEventDispatcher($this->app->make('Illuminate\Contracts\Events\Dispatcher'));
+
+            return $connection;
+        });
+    }
+}
