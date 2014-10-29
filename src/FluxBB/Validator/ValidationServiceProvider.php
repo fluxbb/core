b56f0760ef27eb4ebbe97dd359c5ab99b3644ba5
Franz Liedke <franz@develophp.org>
2014-10-29 09:47:19 +0100

    Add validation service provider that registers custom validation rules.

---
+++ src/FluxBB/Validator/ValidationServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace FluxBB\Validator;
+
+use Illuminate\Contracts\Validation\Factory;
+use Illuminate\Support\ServiceProvider;
+
+class ValidationServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+        $this->app->extend('Illuminate\Contracts\Validation\Factory', function (Factory $factory) {
+            $factory->extend('username_not_guest', function ($attribute, $value) {
+                return strcasecmp($value, 'Guest') && strcasecmp($value, trans('fluxbb::common.guest'));
+            });
+
+            $factory->extend('username_not_reserved', function ($attribute, $value) {
+                return !str_contains($value, array('[', ']', '\'', '"'));
+            });
+
+            $factory->extend('no_ip', function ($attribute, $value) {
+                return !preg_match('%[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}%', $value) &&
+                       !preg_match('%((([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){6}:[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){5}:([0-9A-Fa-f]{1,4}:)?[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){4}:([0-9A-Fa-f]{1,4}:){0,2}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){3}:([0-9A-Fa-f]{1,4}:){0,3}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){2}:([0-9A-Fa-f]{1,4}:){0,4}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){6}((\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b)\.){3}(\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b))|(([0-9A-Fa-f]{1,4}:){0,5}:((\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b)\.){3}(\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b))|(::([0-9A-Fa-f]{1,4}:){0,5}((\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b)\.){3}(\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b))|([0-9A-Fa-f]{1,4}::([0-9A-Fa-f]{1,4}:){0,5}[0-9A-Fa-f]{1,4})|(::([0-9A-Fa-f]{1,4}:){0,6}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){1,7}:))%', $value);
+            });
+
+            return $factory;
+        });
+    }
+}
