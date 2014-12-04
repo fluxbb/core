2fb6bcda2ef51ca5d3e3050d2c2b487c30abf52b
Eric Hocking <xaoseric@users.noreply.github.com>
2014-12-04 09:13:38 -0500

    Add OAuth Clients table

---
+++ src/Migrations/Install/OAuthClients.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace FluxBB\Migrations\Install;
+
+use FluxBB\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class OAuthClients extends Migration
+{
+    /**
+     * @var string
+     */
+    protected $table = 'oauth_clients';
+    protected function create(Blueprint $table)
+    {
+        $table->create();
+        $table->string('client_id', 80);
+        $table->string('client_secret', 80);
+        $table->string('redirect_uri', 2000);
+        $table->string('grant_types', 80);
+        $table->string('scope', 100);
+        $table->string('user_id', 80)
+        $table->timestamps();
+     }
