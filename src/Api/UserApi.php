8f694789782857b3451e7cdf0402acc16f066e3c
Eric Hocking <xaoseric@users.noreply.github.com>
2014-12-16 11:27:07 -0500

    Create the register api function

--- src/Api/UserApi.php
+++ src/Api/UserApi.php
@@ -3,14 +3,47 @@
 
 namespace FluxBB\Api;
 
-class UserApi 
-{
+use FluxBB\Actions\CreateUser;
+use FluxBB\Core\Action; 
+use FluxBB\Events\UserHasRegistered; 
+use FluxBB\Models\User; 
+use FluxBB\Models\ConfigRepositoryInterface; 
 
-/*
- * 
- * This file handles User registration, and login api. 
- * url structure: /api/v1/users/[function]
- *
- */
+
+class UserApi extends Action
+{
+  
+  /*
+   *  
+   * This file handles User registration, and login api. 
+   * url structure: /api/v1/users/[function]
+   *
+   */
+  
+  protected $config; 
+  protected $api_key;
+  
+  public function __construct(ConfigRepositoryInterface $repository) 
+  { 
+    $this->config = $repository; 
+  } 
+
+  /*
+   *
+   * Get the variables username, email, password, groupid, active, api_key from the url to use to register the user
+   * @returns json array indicating success or failure
+   */
+
+  public function action_register($api_key, $username, $email, $password, $group_id, $active)
+  {
+    //first we need to get our api key, and verify it
+    $this->api_key = $api_key;
+    
+    //FluxBB\Api\ApiBase - todo: create function to veify api key from key provided in url
+  
+  }
+
+ 
+ 
 
 }
