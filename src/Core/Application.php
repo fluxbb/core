46a76d1cb613f9133a5ded147faf26b85f8b3cb7
Franz Liedke <franz@develophp.org>
2015-02-04 15:50:12 +0100

    Fix application class (needs to implement application contract).

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -3,17 +3,22 @@
 namespace FluxBB\Core;
 
 use Illuminate\Container\Container;
+use Illuminate\Contracts\Foundation\Application as AppContract;
 
-class Application extends Container
+class Application extends Container implements AppContract
 {
+    protected $basePath;
+
     protected $isBooted = false;
 
     protected $registered = [];
 
 
-    public function __construct()
+    public function __construct($basePath)
     {
         static::setInstance($this);
+
+        $this->basePath = $basePath;
 
         $this->instance('app', $this);
 
@@ -23,11 +28,7 @@ class Application extends Container
         $this->register('Illuminate\Events\EventServiceProvider');
     }
 
-    /**
-     * @param string $provider
-     * @return void
-     */
-    public function register($provider)
+    public function register($provider, $options = array(), $force = false)
     {
         $provider = $this->makeProvider($provider);
 
@@ -62,5 +63,85 @@ class Application extends Container
         }
 
         $this->isBooted = true;
+    }
+
+    public function basePath()
+    {
+        return $this->basePath;
+    }
+
+    /**
+     * Get the version number of the application.
+     *
+     * @return string
+     */
+    public function version()
+    {
+        // TODO: Implement version() method.
+    }
+
+    /**
+     * Get or check the current application environment.
+     *
+     * @param  mixed
+     * @return string
+     */
+    public function environment()
+    {
+        // TODO: Implement environment() method.
+    }
+
+    /**
+     * Determine if the application is currently down for maintenance.
+     *
+     * @return bool
+     */
+    public function isDownForMaintenance()
+    {
+        // TODO: Implement isDownForMaintenance() method.
+    }
+
+    /**
+     * Register all of the configured providers.
+     *
+     * @return void
+     */
+    public function registerConfiguredProviders()
+    {
+        // TODO: Implement registerConfiguredProviders() method.
+    }
+
+    /**
+     * Register a deferred provider and service.
+     *
+     * @param  string $provider
+     * @param  string $service
+     * @return void
+     */
+    public function registerDeferredProvider($provider, $service = null)
+    {
+        // TODO: Implement registerDeferredProvider() method.
+    }
+
+    /**
+     * Register a new boot listener.
+     *
+     * @param  mixed $callback
+     * @return void
+     */
+    public function booting($callback)
+    {
+        // TODO: Implement booting() method.
+    }
+
+    /**
+     * Register a new "booted" listener.
+     *
+     * @param  mixed $callback
+     * @return void
+     */
+    public function booted($callback)
+    {
+        // TODO: Implement booted() method.
     }
 }
